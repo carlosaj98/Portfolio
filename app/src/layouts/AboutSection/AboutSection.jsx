49d5e84009f950baa173ca22c5c9e5b38cf2d2fa
carlosaj98 <carlosaj.arahal@gmail.com>
2023-11-30 12:00:43 +0100

    feat: new icons svg

--- app/src/layouts/AboutSection/AboutSection.jsx
+++ app/src/layouts/AboutSection/AboutSection.jsx
@@ -1,8 +1,14 @@
-import { Stack, Typography } from "@mui/material"
+import { Box, Stack, Typography } from "@mui/material"
 
 function AboutSection() {
   return (
-    <Stack component={"section"} height={"100vh"} color={"white"} id="about-section" alignItems={"center"} >
+    <Stack
+      component={"section"}
+      height={"100vh"}
+      color={"white"}
+      id="about-section"
+      alignItems={"center"}
+    >
       <Typography variant="h3">About</Typography>
     </Stack>
   )
